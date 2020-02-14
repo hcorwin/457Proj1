597c212c48fb2b5f181f3d2885975bd573203bc5
hcorwin <43582690+hcorwin@users.noreply.github.com>
2020-02-14 15:09:43 -0500

    Update ftp_client.py

--- ftp_client.py
+++ ftp_client.py
@@ -1,25 +1,66 @@
-import ftplib
+import socket
+import sys
+import os
+import struct
 
+HOST = "127.0.0.1"  # The server's hostname or IP address
+PORT = 65432        # The port used by the server
+BUFFER = 1024
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-def upload(ftp, filename):
-    ftp.storlines("STOR " + filename, open(filename))
+def conn():
+    s.connect((HOST, PORT))
+    print("You have been connected")
 
+def upload(fullcommand):
+    s.send(fullcommand.encode('utf-8'))
+    commands = fullcommand.split(' ', 1)
+    file = commands[1]
+    with open(file, 'rb') as infile:
+        for data in infile:
+            s.sendall(data)
+    infile.close()
+    print("File uploaded")
 
-def download(ftp, filename):
-    try:
-        ftp.retrbinary("RETR " + filename, open(filename, 'wb').write)
-    except:
-        print("Error, unable to download")
+def givelist():
+    print("Here is your list")
 
+def download(fullcommand):
+    s.send(fullcommand.encode('utf-8'))
+    commands = fullcommand.split(' ', 1)
+    file = commands[1]
+    with open(file, 'wb') as outfile:
+        while True:
+            data = s.recv(1024)
+            if not data:
+                break
+            outfile.write(data)
+            print("loop2")
+        outfile.close()
+    print("File downloaded")
 
-def listall(ftp):
-    ftp.retrlines('LIST')
+def discon(fullcommand):
+    s.send(fullcommand.encode())
+    s.close()
+    print("Disonnected")
 
-
-def disconnect(ftp):
-    ftp.quit()
-
-
-def connect(ftp):
-    ftp = ftplib.FTP("127.0.0.1")
-    ftp.login("user", "123")
+while True:
+    str = input("Command?:\n")
+    commands = str.split(' ', 1)
+    if commands[0] == "QUIT":
+        print("Goodbye")
+        discon(str)
+        quit()
+        break
+    elif commands[0] == "CONN":
+        conn()
+    elif commands[0] == "UPLD":
+        upload(str)
+        print("UPPPPP")
+    elif commands[0] == "LIST":
+        givelist()
+    elif commands[0] == "DWLD":
+        download(str)
+        print("DOWNNNNNN")
+    else:
+        print("INVALID COMMAND")
