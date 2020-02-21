dc1b11f94c93e6dbe8f359ce409b8af7369382b4
hcorwin <43582690+hcorwin@users.noreply.github.com>
2020-02-20 22:30:38 -0500

    Update ftp_server.py
    error handling for files added

--- server/ftp_server.py
+++ server/ftp_server.py
@@ -1,6 +1,6 @@
 import socket
-import os
 import sys
+import os
 
 host = "127.0.0.1"  # The server's hostname or IP address
 port = 65432  # The port used by the server
@@ -10,40 +10,41 @@ socket.bind((host, port))  # Bind to port
 socket.listen(1)  # Wait for client connection
 
 while True:
-    print("Waiting to establish connection with someone...")
-
     # Establish connection with client
     connection, address = socket.accept()
-    print("\nConnected with client -", address)
+    print("Connected with client -", address)
 
     while True:
         # Receive Command from client
         command = connection.recv(1024)
-        print("\nCommand received: ", command.decode('UTF-8'))
+        print("Command received: ", command.decode('UTF-8'))
 
         if (command.decode('UTF-8') == 'QUIT'):
-            # Close connection with client and then close the server
-            print("\nSee you again client -", address)
+            print("See you again client -", address)
             connection.close()
             break
-
-        # Send list of files in the server
+            # Send list of files in the server
+        elif (command.decode('UTF-8') == 'HMANY'):
+            data = os.listdir()
+            count = 0
+            for line in data:
+                count += 1
+            connection.send(str(count).encode('UTF-8'))
         elif (command.decode('UTF-8') == 'LIST'):
             data = os.listdir()
             print("Sending directory list:")
             for line in data:
                 print(">", line)
                 connection.send(line.encode('UTF-8'))
-                connection.send(' '.encode('UTF-8'))
-
+                #connection.send(' '.encode('UTF-8'))
         else:
             # Tokenize command and save filename
             commands = command.decode().split(' ', 1)
             file = commands[1]
+            print(file)
 
             # Receive incoming data, write to file and upload file to the server
             if (commands[0] == 'UPLD'):
-                print("Uploading", file, "--- Please be patient!")
                 with open(file, 'w') as writefile:
                     while True:
                         data = connection.recv(1024)
@@ -52,16 +53,21 @@ while True:
                             break
                         writefile.write(data.decode('UTF-8'))
                         writefile.close()
+                        print(data)
                         break
 
             # Send all data from file to client to download
-            elif (commands[0] == 'RETRIEVE'):
-                print("Downloading", file)
-                with open(file, 'r') as readfile:
-                    for data in readfile:
-                        connection.sendall(data.encode('UTF-8'))
-                    readfile.close()
-                    break
+            elif (commands[0] == 'DWLD'):
+                data = os.listdir()
+                if file in data:
+                    with open(file, 'r') as readfile:
+                        for data in readfile:
+                            connection.sendall(data.encode('UTF-8'))
+                else:
+                    print("File not found")
+                    connection.send("no".encode('UTF-8'))
+
     break
 
 socket.close()
+
